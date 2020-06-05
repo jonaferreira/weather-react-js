babf84dd36ab6fbcf6d480e12828a912cc7021f8
FERREIRA, JONATHAN IVAN <joferreira@santandertecnologia.com.ar>
2020-06-05 01:15:55 -0300

    Updates

---
+++ precache-manifest.86b50bdc93d29b3764a507ad7cc73764.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8d19293682db2c14e7b6ede993955e",
+    "url": "/weather-react-js/index.html"
+  },
+  {
+    "revision": "808f8281a015317f544c",
+    "url": "/weather-react-js/static/css/main.fc7720c3.chunk.css"
+  },
+  {
+    "revision": "a13deb69ae4f79be7e25",
+    "url": "/weather-react-js/static/js/2.631cd937.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-react-js/static/js/2.631cd937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808f8281a015317f544c",
+    "url": "/weather-react-js/static/js/main.dba37e55.chunk.js"
+  },
+  {
+    "revision": "cb005690e06728f7235c",
+    "url": "/weather-react-js/static/js/runtime-main.0a713463.js"
+  },
+  {
+    "revision": "b521ebc69a6107a4bf9d4c0cd7731c2d",
+    "url": "/weather-react-js/static/media/pexels-photo-2682103.b521ebc6.jpeg"
+  }
+]);
